9f46def41d517b2365e9df0be0d23678704b2966
nikhil <nikhil.parlapalli@magna.com>
2019-10-01 22:31:51 -0400

    Added src and dst points roughly. Complete the project and then check if right

--- project2.py
+++ project2.py
@@ -114,33 +114,21 @@ def region_of_interest(img, vertices):
 def corners_unwarp(img, nx, ny, mtx, dist):
     # Use the OpenCV undistort() function to remove distortion
     undist = cv2.undistort(img, mtx, dist, None, mtx)
-    # Convert undistorted image to grayscale
-    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
-    # Search for corners in the grayscaled image
-    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+    
+    img_size = (img.shape[1], img.shape[0])
 
-    if ret == True:
-        # If we found corners, draw them! (just for fun)
-        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
-        # Choose offset from image corners to plot detected corners
-        # This should be chosen to present the result at the proper aspect ratio
-        # My choice of 100 pixels is not exact, but close enough for our purpose here
-        offset = 100 # offset for dst points
-        # Grab the image shape
-        img_size = (gray.shape[1], gray.shape[0])
-
-        # For source points I'm grabbing the outer four detected corners
-        src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
-        # For destination points, I'm arbitrarily choosing some points to be
-        # a nice fit for displaying our warped result 
-        # again, not exact, but close enough for our purposes
-        dst = np.float32([[offset, offset], [img_size[0]-offset, offset], 
-                                     [img_size[0]-offset, img_size[1]-offset], 
-                                     [offset, img_size[1]-offset]])
-        # Given src and dst points, calculate the perspective transform matrix
-        M = cv2.getPerspectiveTransform(src, dst)
-        # Warp the image using OpenCV warpPerspective()
-        warped = cv2.warpPerspective(undist, M, img_size)
+    # For source points I'm grabbing the outer four detected corners
+    #src = np.float32([[250,683],[500,510], [766,510], [1042,683]])
+    src = np.float32([[250,683],[350,608], [920,608], [1042,683]])
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    #dst = np.float32([[250,683], [250,510], [1042,510], [1042,683]])
+    dst = np.float32([[250,683], [250,608], [1042,608], [1042,683]])
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv2.warpPerspective(undist, M, img_size)
 
     # Return the resulting image and matrix
     return warped, M
@@ -150,9 +138,12 @@ img = cv2.imread('./test_images/straight_lines1.jpg')
 img_size = (img.shape[1], img.shape[0])
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
 col_grad = color_and_gradient(img)
-#warped,perspective_M  = corners_unwarp(col_grad, nx, ny, mtx, dist)
+warped1,perspective_M  = corners_unwarp(col_grad, nx, ny, mtx, dist)
 
-result = col_grad
+img = cv2.imread('./test_images/straight_lines2.jpg')
+warped,perspective_M  = corners_unwarp(col_grad, nx, ny, mtx, dist)
+
+result = warped1
 # use (arr, cmap='gray') option when you want to show a grey scale image 
 plt.imshow(result, cmap='gray')
 plt.show()
